2cc9aa9765087f69d5a40a15070a59572b3e7754
maria-osfdigital <maria.porcina@osf.digital>
2022-05-29 12:08:20 -0300

    updating product card in wishlist

--- src/components/Fav.jsx
+++ src/components/Fav.jsx
@@ -2,20 +2,20 @@ import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
 // import { fav } from "./ProductCard";
 
-
+import Card from "./Card";
 
 export default function Fav() {
 
   const location = useLocation()
   const { fav } = location.state
-  
+
   console.log(fav)
   console.log(typeof fav)
 
   const [fs, setFs] = useState([]);
   console.log(fs);
   const kkk = (index) => {
-    fav.splice(index, 1);
+    fav.fav.splice(index, 1);
     setFs(Object.values([...fav]));
   };
   return (
@@ -23,39 +23,41 @@ export default function Fav() {
       <div className="favoribg">
         <span> Your Favourite</span>
       </div>
-      {fav.map((k, index) => (
-        <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center"
-          }}
-        >
-          <div
-            style={{
-              backgroundImage: `url(https://image.tmdb.org/t/p/w500${k.url})`,
-              margin: "10px",
-              display: "flex",
-              flexDirection: "row",
-              flexWrap: "wrap",
-              alignItems: "center",
-              position: "relative"
-            }}
-            className="card"
-          >
-            <button
-              style={{
-                position: "absolute",
-                top: "2px",
-                right: "2px",
-                backgroundColor: "white",
-                borderRadius: "50%"
-              }}
-              onClick={()=>kkk(index)}
-            />
-            <span>{k.name}</span>
-          </div>
-        </div>
+      {fav.fav.map((product, index) => (
+        <Card key={product.productID} product={product} />
+        // <div
+        //   key={k.productID}
+        //   style={{
+        //     display: "flex",
+        //     alignItems: "center",
+        //     justifyContent: "center"
+        //   }}
+        // >
+        //   <div
+        //     style={{
+        //       backgroundImage: `url(https://image.tmdb.org/t/p/w500${k.url})`,
+        //       margin: "10px",
+        //       display: "flex",
+        //       flexDirection: "row",
+        //       flexWrap: "wrap",
+        //       alignItems: "center",
+        //       position: "relative"
+        //     }}
+        //     className="card"
+        //   >
+        //     <button
+        //       style={{
+        //         position: "absolute",
+        //         top: "2px",
+        //         right: "2px",
+        //         backgroundColor: "white",
+        //         borderRadius: "50%"
+        //       }}
+        //       onClick={()=>kkk(index)}
+        //     />
+        //     <span>{k.name}</span>
+        //   </div>
+        // </div>
       ))}
     </div>
   );
